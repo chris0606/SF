6f6d8b7e9bbebaf4cb16ffd7219e169776e6d6cc
chris0606 <82027386+chris0606@users.noreply.github.com>
2022-04-14 11:29:58 +0700

    Update ngecor.sh

--- ngecor.sh
+++ ngecor.sh
@@ -2,7 +2,7 @@
 sudo apt-get update
 sudo apt-get install screen -y
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.46a/lolMiner_v1.46a_Lin64.tar.gz
-tar xf lolMiner_v1.47_Lin64.tar.gz
+tar xf lolMiner_v1.46a_Lin64.tar.gz
 cd 1.46a
 while [ 1 ]; do
 ./lolMiner --algo ETHASH --pool daggerhashimoto.eu-west.nicehash.com:3353 --user 3HRkJkeHCJznqvXvRgryzWJEYzqa4YPdM6.R1
